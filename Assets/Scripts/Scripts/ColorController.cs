c26736e2104fd4c20468f2e46968771a85900d96
kadirefetufan <kadirefetufan@gmail.com>
2024-01-26 17:19:30 +0300

    Ui Fix

--- Assets/Scripts/Scripts/ColorController.cs
+++ Assets/Scripts/Scripts/ColorController.cs
@@ -1,22 +1,27 @@
+using Enums;
 using UnityEngine;
 
 namespace Controllers
 {
     public class ColorController : MonoBehaviour
     {
+        public ColorTypes ColorType;
+
         [SerializeField] private MeshRenderer meshRenderer;
-        [SerializeField] private Material newMaterial; // Elle atayabileceðiniz material
+        [SerializeField] private Material[] materials; // Malzemeleri burada saklayýn
 
         private void Awake()
         {
-            ChangeMaterial(newMaterial);
+            ChangeAreaColor(ColorType);
         }
 
-        public void ChangeMaterial(Material material)
+        public void ChangeAreaColor(ColorTypes _colorType)
         {
-            if (material != null && meshRenderer != null)
+            // ColorTypes enum deðerine göre malzemeyi seçin ve atayýn
+            Material selectedMaterial = materials[(int)_colorType];
+            if (selectedMaterial != null)
             {
-                meshRenderer.material = material;
+                meshRenderer.material = selectedMaterial;
             }
         }
     }
